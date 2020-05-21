4b9ce5dd65e9c4b1312ef9ec721ed267b8cf87d6
Jonathan-glitch <Jona32839@gmail.com>
2020-05-21 11:38:13 -0400

    added funcionability to commands and it has verry basic function

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,27 @@
 // ===============================================================================
-// DATA
-// Below data will hold all of the waitlist tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
+// Array of 'friend' objects that will be accessed via /api/friends route
 // ===============================================================================
 
 var friendsArray = [
-    
+  {
+    name: "Ahmed",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+  },
+  {
+    name: "Elie",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shhttps://images.pexels.com/photos/936119/pexels-photo-936119.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260rinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+  },
+  {
+    name: "Elie",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shhttps://images.pexels.com/photos/936119/pexels-photo-936119.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260rinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1],
+  },
 ];
-  
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friendsArray;
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
